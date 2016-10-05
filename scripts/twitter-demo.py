0c93374b777e3d238d4eb1f96b5e8e672eb84d7b
Christophe Alexandre <chris.perso@gmail.com>
2016-10-05 09:34:01 +0200

    dev

--- scripts/twitter-demo.py
+++ scripts/twitter-demo.py
@@ -1,12 +1,11 @@
 import argparse
+from datetime import datetime
 import json
 import logging
 import os
 
 from twitter import OAuth
-from twitter import OAuth2
 from twitter import Twitter
-from twitter import oauth2_dance
 from twitter import oauth_dance
 from twitter import read_token_file
 
@@ -49,18 +48,23 @@ def main():
     twitter_token_filename = config_json['twitter_token_filename']
 
     if not os.path.isfile(twitter_token_filename):
-        oauth_token, oauth_token_secret = '', ''
+        oauth_dance('announcements-app', consumer_token, consumer_secret, twitter_token_filename)
 
-    else:
-        oauth_token, oauth_token_secret = read_token_file(twitter_token_filename)
+    oauth_token, oauth_token_secret = read_token_file(twitter_token_filename)
 
-    twitter = Twitter(auth=OAuth(oauth_token, oauth_token_secret, consumer_token, consumer_secret))
-    timeline = twitter.statuses.home_timeline()[0]
+    twitter_service = Twitter(auth=OAuth(oauth_token, oauth_token_secret, consumer_token, consumer_secret))
+    target_folder_id = config_json['target_folder_id']
+    publish_twitter(twitter_service, target_folder_id)
+
+
+def publish_twitter(twitter_service, target_folder_id):
+    timeline = twitter_service.statuses.home_timeline()[0]
     logging.info('{}: {}'.format(timeline['user'], timeline['text']))
-
-    message = """#InteractiveBrokers mapping conId vs instruments available for stocks and ETFS
-    https://drive.google.com/open?id={}""".format(config_json['target_folder_id'])
-    twitter.statuses.update(status=message)
+    message = '#InteractiveBrokers mapping conId vs instruments available for stocks and ETFS: {} {} update'
+    dir_url = 'https://drive.google.com/open?id={}'.format(target_folder_id)
+    month = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')[datetime.today().month - 1]
+    twitter_service.statuses.update(status=message.format(dir_url, month))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
